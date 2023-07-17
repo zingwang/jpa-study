52a9eceb90bc2d2b1c78cd83fa39a7dd17a5d12b
zingwang <zingwang06@naver.com>
2023-07-17 15:41:32 +0900

    엔티티클래스 Member

---
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name="member_id")
+    private Long id;
+
+    private String name;
+
+    private Address address;
+
+    @OneToMany(mappedBy="member")//읽기전용
+    private List<Order> orders = new ArrayList<>();
+
+
+
+
+
+}
