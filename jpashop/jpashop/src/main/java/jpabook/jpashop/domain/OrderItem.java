bf3f66bb382c947e80053c265b49486365919ef2
zingwang <zingwang06@naver.com>
2023-07-24 21:13:47 +0900

    주문 서비스 개발

--- jpashop/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,12 +2,15 @@ package jpabook.jpashop.domain;
 
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
--- jpashop/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,8 +1,8 @@
 package jpabook.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.criteria.Order;
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
