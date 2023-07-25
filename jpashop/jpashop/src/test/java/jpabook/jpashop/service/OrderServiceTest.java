14a916b606f37be1a70d0a2eaff27300306c9a68
zingwang <zingwang06@naver.com>
2023-07-25 20:51:24 +0900

    주문취소 테스트코드

--- jpashop/jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ jpashop/jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -28,12 +28,11 @@ public class OrderServiceTest {
     @Test
     public void 상품주문() throws  Exception{
         Member member = createMember();
-
         Book book = createBook("시골 JPA",10000,10);
-
-        //when
         int orderCount=2;
-       Long orderId= orderService.order(member.getId(), book.getId(),orderCount);
+        //when
+
+        Long orderId= orderService.order(member.getId(), book.getId(),orderCount);
 
         //then
         Order getOrder = orderRepository.findOne(orderId);
@@ -78,10 +77,19 @@ public class OrderServiceTest {
     @Test
     public void 주문취소() throws  Exception{
         //given
+        Member member = createMember();
+        Item item = createBook("시골 JPA",10000,10);
+        int orderCount=2;
+        Long orderId= orderService.order(member.getId(), item.getId(), orderCount);
 
         //when
+        orderService.cancelOrder(orderId);
 
         //then
+
+        Order getOrder = orderRepository.findOne(orderId);
+        Assert.assertEquals("상품주문시 상태는 CANCEL", OrderStatus.CANCEL, getOrder.getStatus());
+        Assert.assertEquals("주문이 취소된 상품은 그만큼 재고가 증가", 10, item.getStockQuantity());
     }
 
 
