1a334d1bc6c155cfc9c78d3b987cc65f2fa159c2
zingwang <zingwang06@naver.com>
2023-07-25 21:11:31 +0900

    주문 검색기능 리포지토리

--- jpashop/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,11 +1,15 @@
 package jpabook.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -21,6 +25,18 @@ public class OrderRepository {
     public Order findOne(Long id){
         return em.find(Order.class,id);
     }
+
+    public List<Order> findAll(OrderSearch orderSearch){
+
+       return em.createQuery("select o from Order o join o.member m"+
+                " where o.status = :status"+
+                " and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000)
+                .getResultList();
+    }
+
 
 }
 
