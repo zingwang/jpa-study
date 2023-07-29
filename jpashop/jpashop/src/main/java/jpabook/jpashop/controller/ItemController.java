181970ac0af30fcdc65c9b76400b3734a94b5a33
zingwang <zingwang06@naver.com>
2023-07-29 11:31:18 +0900

    상품리스트

--- jpashop/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.controller;
 
 
+import jpabook.jpashop.domain.Item;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -33,6 +36,12 @@ public class ItemController {
         itemService.saveItem(book);
 
         return "redirect:/items";
+    }
 
+    @GetMapping("/items")
+    public String list(Model model){
+        List<Item> items= itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
     }
 }
