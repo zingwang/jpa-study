2b454f7e6cc7cca095a5cba00a110d23d0d6039f
zingwang <zingwang06@naver.com>
2023-08-08 19:46:32 +0900

    회원 조회 API

--- jpashop/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -7,12 +7,41 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
 
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
+    @GetMapping("/api/v2/members")
+    public Result membersV2(){
+
+
+    List<MemberDto> collect=
+        memberService.findMembers().
+        stream().
+        map(m->new MemberDto(m.getName())).
+                collect(Collectors.toList());
+        return new Result(collect);
+    }
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody Member member){
@@ -28,8 +57,6 @@ public class MemberApiController {
         member.setName(request.getName());
         Long id=memberService.join(member);
         return new CreateMemberResponse(id);
-
-
     }
 
     @PutMapping("/api/v2/members/{id}")
@@ -41,13 +68,6 @@ public class MemberApiController {
 
         return new UpdateMemberResponse(id, memberService.findOne(id).getName());
 
-/*
-        Member member= new Member();
-        member.setName(request.getName());
-        Long id=memberService.join(member);
-        return new CreateMemberResponse(id);
-
- */
     }
 
 
