62ca371d478f236b6de3f1860f8e25bc24ccfeff
zingwang <zingwang06@naver.com>
2023-08-12 21:35:12 +0900

    컬렉션 조회 최적화: 주문조회 v1 엔티티 직접노출

---
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.OrderSimpleQueryRepository;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+
+        for (Order order : all) {
+            order.getMember().getName(); //Lazy 강제 초기화
+            order.getDelivery().getAddress(); //Lazy 강제 초기화
+            
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o-> o.getItem().getName());
+        }
+        return all;
+    }
+
+}
