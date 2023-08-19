450e38d54321ab3c0fb8cbb72beee720f6f727be
zingwang <zingwang06@naver.com>
2023-08-19 11:25:18 +0900

    순수 JPA 기반 리포지토리 생성

--- data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
@@ -16,6 +19,22 @@ public class MemberJpaRepository {
         return member;
     }
 
+    public void delete(Member member){
+        em.remove(member);
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m",Member.class)
+                .getResultList();
+    }
+    public Optional<Member> findById(Long id){
+        Member member= em.find(Member.class,id);
+        return Optional.ofNullable(member);
+    }
+    public long count(){
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
     public Member find(Long id){
         return em.find(Member.class,id);
     }
--- data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -25,4 +27,30 @@ class MemberJpaRepositoryTest {
         assertEquals(member, findMember);
         assertEquals(member.getUsername(), findMember.getUsername());
     }
+
+
+    @Test
+    public void basicCRUD(){
+        Member member1= new Member("member1");
+        Member member2= new Member("member1");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+        //단건 조회 검증
+        Member findMember1 =  memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        assertEquals(findMember1,member2);
+        assertEquals(findMember2,member2);
+        //리스트 조회 검증
+        List<Member> all = memberJpaRepository.findAll();
+        assertEquals(all.size(),2);
+        //카운트 검증
+        long count = memberJpaRepository.count();
+        assertEquals(count,2);
+        //삭제 검증
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+        long deletedCount = memberJpaRepository.count();
+        assertEquals(deletedCount,0);
+    }
+
 }
