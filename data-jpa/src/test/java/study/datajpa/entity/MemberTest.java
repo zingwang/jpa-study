9de078a3b08b7a280675af60ed6d5ccc8b6ea729
zingwang <zingwang06@naver.com>
2023-08-31 20:29:57 +0900

    스프링 데이터 JPA - Auditing:JPA

--- data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
+++ data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -3,15 +3,19 @@ package study.datajpa.entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
 
 import java.util.List;
 @SpringBootTest
 public class MemberTest {
-    @PersistenceContext
+    @Autowired
     EntityManager em;
+    @Autowired
+    MemberRepository memberRepository;
     @Test
     @Transactional
     @Rollback(false)
@@ -39,5 +43,24 @@ public class MemberTest {
             System.out.println("member=" + member);
             System.out.println("-> member.team=" + member.getTeam());
         }
+    }
+
+    @Test
+    @Transactional
+    public void JpaEventBaseEntity() throws InterruptedException {
+        //given
+        Member member=new Member("member1");
+        memberRepository.save(member); //@PrePersist
+
+        member.setUsername("memberEdit");
+        Thread.sleep(100);
+        em.flush(); // @PreUpdate
+        em.clear();
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        //then
+        System.out.println("findMember createDate = " + findMember.getCreateDate());
+        System.out.println("findMember updateDate= " + findMember.getUpdateDate());
     }
 }
