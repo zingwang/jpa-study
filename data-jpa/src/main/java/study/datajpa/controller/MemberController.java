510afaa3bcfdf2ed54ea0ef2c28b7a13605db2b7
zingwang <zingwang06@naver.com>
2023-09-02 13:17:28 +0900

    스프링 데이터 JPA - Web 확장 - 페이징과 정렬

--- data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -3,9 +3,13 @@ package study.datajpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -24,11 +28,19 @@ public class MemberController {
     @GetMapping("/members2/{id}")
     public String findMember2(@PathVariable("id") Member member){
         return member.getUsername();
+    }
 
+    //members?page=0&size=3&sort=id ..
+    @GetMapping("members")
+    public Page<MemberDto> list(@PageableDefault(size=5) Pageable pageable){
+
+        return memberRepository.findAll(pageable).map(MemberDto::new);
     }
 
     @PostConstruct
     public void init(){
-        memberRepository.save(new Member("userA"));
+        for(int i=0; i<100; i++){
+            memberRepository.save(new Member("user"+i));
+        }
     }
 }
